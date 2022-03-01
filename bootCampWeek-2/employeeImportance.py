afae456495d9c413851f80e4ec8990ef23a0cc15
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-03-02 05:14:32 +0900

    Create employeeImportance.py

---
+++ bootCampWeek-2/employeeImportance.py
@@ -0,0 +1,29 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+def idFinder(id, employees):
+    for l in employees:
+        if l.id == id:
+            return l
+        
+def subs(i: 'Employee', employees):
+    sum=0
+    for j in i.subordinates:
+        sum+=subs(idFinder(j,employees),employees)
+        
+    return sum + i.importance
+    
+    
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        
+        for l in employees:
+            if l.id == id:
+                return subs(l,employees)
+               
+                
