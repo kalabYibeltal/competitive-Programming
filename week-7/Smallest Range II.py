329424fa5a321fa6e82e653a471d9f59a740cbea
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-03-30 23:46:06 +0900

    Delete Smallest Range II.py

--- week-7/Smallest Range II.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def smallestRangeII(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        ans = nums[-1] - nums[0]
-        for i in range(0,len(nums)-1):
-            ans = min(ans, (max(nums[-1]-k, nums[i] +k) - min(nums[0]+k,nums[i+1]-k)))
-        return ans
