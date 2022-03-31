516914260ef3f18767996d0916783bbcd58633b3
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-04-01 04:18:39 +0900

    Delete countGoodNumbers.py

--- countGoodNumbers.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        if n%2==0:  
-            return int((pow(5,int(n/2),1000000007)*pow(4,int(n/2),1000000007))%(1000000007))        
-        else:
-            return int((pow(5,int((n+1)/2),1000000007)*pow(4,int(n/2),1000000007))%(1000000007))
