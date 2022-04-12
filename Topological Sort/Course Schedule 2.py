15c8e24757e26bed1ae36c5a9247e7c21d9b4467
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-04-12 22:36:15 +0900

    Create Course Schedule 2.py

---
+++ Topological Sort/Course Schedule 2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dictr = {}
+        arr = [0] * numCourses
+        output = []
+        for i in prerequisites:
+            arr[i[0]] += 1
+        for i in prerequisites:
+            if i[1] in dictr:
+                dictr[i[1]].append(i[0])
+            else: dictr[i[1]]=[i[0]]
+        # print(dictr)       
+        que = collections.deque()
+        for i in range(len(arr)):
+            if arr[i] == 0: que.append(i)
+        # print(que, arr)
+        while que:
+            val = que.popleft()
+            output.append(val)
+            # print(val)
+            if val in dictr:
+                for i in dictr[val]:
+                    arr[i] -= 1
+                    if arr[i]==0: que.append(i)
+        # print(output)
+        if len(output) < numCourses: return []
+        return output
