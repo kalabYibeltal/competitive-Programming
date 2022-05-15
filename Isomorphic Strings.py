033cb49e889ddccee29f1864e2e5696a6d236529
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-05-15 13:18:05 +0900

    Create Isomorphic Strings.py

---
+++ Isomorphic Strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dicts = {}
+        dictt = {}
+        dic = {}
+        for i in range(len(s)):
+            if s[i] in dicts: 
+                if t[i] in dictt and dictt[t[i]] == s[i]: 
+                    dicts[s[i]][0].append(i)
+                    dicts[s[i]][1].append(i)
+                else: return 
+            elif t[i] in dictt: return    
+            else: 
+                dictt[t[i]] = s[i]
+                dicts[s[i]] = [[i],[i]]
+                
+        print(dicts)        
+        return True
