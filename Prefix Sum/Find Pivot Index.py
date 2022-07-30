06ee5839ef059f2d0e98e39b970cac3464c2376e
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2022-07-30 17:12:03 -0400

    Update Find Pivot Index.py

--- Prefix Sum/Find Pivot Index.py
+++ Prefix Sum/Find Pivot Index.py
@@ -6,3 +6,14 @@ class Solution:
             if su == (s - nums[i])/2: return i
             su += nums[i]
         return -1
+  
+# another trial
+# class Solution:
+#     def pivotIndex(self, nums: List[int]) -> int:
+#         s = sum(nums)
+#         r = 0
+#         for i , num in enumerate(nums):
+#             if r == s - num - r: return i 
+#             r += num
+            
+#         return -1
