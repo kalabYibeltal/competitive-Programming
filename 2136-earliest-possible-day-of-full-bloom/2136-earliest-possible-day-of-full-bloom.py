3fff84bd334b1e46e3324689a8a1e7c0b39b66c4
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2023-02-01 10:23:28 +0300

    Time: 1932 ms (52.48%), Space: 34 MB (42.24%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -1,11 +1,12 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        A=[[i,j] for i ,j in zip(plantTime,growTime)]
-        A=sorted(A,key=lambda x:x[1],reverse=True)
-        
+        newArray = []
+        for i in range(len(plantTime)):
+            newArray.append([plantTime[i],growTime[i]])
+        newArray=sorted(newArray,key=lambda x:x[1],reverse=True)
         ans=-1
-        date=-1
-        for i , j in A:
-            date+=i
-            ans=max(ans,date+j+1)
+        day=-1
+        for p , q in newArray:
+            day+=p
+            ans=max(ans,day+q+1)
         return ans
