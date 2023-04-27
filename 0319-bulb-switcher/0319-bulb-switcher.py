a5b119502b667f4ca9df829317b6de9a67704af5
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2023-04-27 14:48:10 -0400

    Time: 58 ms (8.42%), Space: 16.2 MB (34.09%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -1,5 +1,8 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
+        ## georg answer
+        int(pow(n,0.5))
+        ### my answer
         if n == 0: return 0
         counter = 3
         adder = 3
