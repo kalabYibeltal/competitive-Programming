61c0550c4b1192ad8bb11a7b55554ee8c95bc04b
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2023-08-10 09:15:53 -0400

    Time: 239 ms (91.31%), Space: 20.3 MB (47.65%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+
+        heater_ptr = 0
+        answer = 0 
+
+        for house in houses:
+            dist = abs(house - heaters[heater_ptr])
+
+            for index in range(heater_ptr + 1, len(heaters)):
+                dist2 = abs(house - heaters[index])
+
+                if dist2 > dist:
+                    break
+
+                else:
+                    dist = dist2
+                    heater_ptr = index
+            answer = max (answer, dist)
+
+        return answer
