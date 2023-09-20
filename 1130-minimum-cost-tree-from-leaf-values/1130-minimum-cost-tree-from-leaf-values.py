259c762ce9a04e533b6af21a8f9b31db182cba38
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2023-09-20 04:11:34 -0400

    Time: 43 ms (71.72%), Space: 16.2 MB (95.71%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -12,19 +12,9 @@ class Solution:
                 if sol[i] * sol[i+1] < tot:
                     tot = sol[i] * sol[i+1]
                     index = i
+            solution += sol[index]*sol[index+1]
+            sol = sol[:index] + [max(sol[index],sol[index+1])] + sol[index+2:]
             
-            new_sol = []
-            i = 0
-            while i < len(sol):
-                if i == index:
-                    new_sol.append(max(sol[i],sol[i+1]))
-                    solution += sol[i]*sol[i+1]
-                    i += 2  
-                else:
-                    new_sol.append(sol[i])
-                    i += 1
-            
-            sol = new_sol
             
         return solution
                 
