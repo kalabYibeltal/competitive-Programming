a99dad7388507eab41dd6e60e7ce918f32017766
Kalab Yibeltal Assefa <60613779+kalabYibeltal@users.noreply.github.com>
2023-12-03 17:42:24 +0300

    Time: 1253 ms (99.63%), Space: 43.9 MB (86.45%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,13 +1,25 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        mem = defaultdict(int)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                mem[(i,grid[i][j], "r")] += 1
-                mem[(j,grid[i][j], "c")] += 1
+#         mem = defaultdict(int)
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 mem[(i,grid[i][j], "r")] += 1
+#                 mem[(j,grid[i][j], "c")] += 1
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                grid[i][j] = mem[(i,1, "r")] + mem[(j,1, "c")] - mem[(i,0, "r")] - mem[(j,0, "c")]
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 grid[i][j] = mem[(i,1, "r")] + mem[(j,1, "c")] - mem[(i,0, "r")] - mem[(j,0, "c")]
         
+        # return grid
+    
+        ### online sub
+        m, n = len(grid), len(grid[0])
+        def count(nums) : return 2*sum(nums)-len(nums)       # [1] count score for a list of numbers
+            
+        rows = list(map(count, grid))                        # [2] count scores for rows
+        cols = list(map(count, zip(*grid)))                  # [3] count scores for columns (transpose)
+
+        for i,j in product(range(m), range(n)):              # [4] update the input matrix with
+            grid[i][j] = rows[i] + cols[j]                   #     sums of precomputed scores
+                
         return grid
